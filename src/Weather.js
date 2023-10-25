a2dca6fbbd958ebe3d6451913a941dd35e202f04
kyasikaze <catarinapsantos98@gmail.com>
2023-10-25 11:46:41 +0200

    Added API response and github link

--- src/Weather.js
+++ src/Weather.js
@@ -1,25 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import { Vortex } from "react-loader-spinner";
+import "./App.css";
 
-export default function Weather(props) {
+export default function Weather() {
+  let [city, setCity] = useState("");
+  let [loaded, setLoaded] = useState(false);
+  let [weather, setWeather] = useState({});
   function displayForecast(response) {
-    alert(
-      `The weather in ${response.data.name} is ${response.data.main.temp}°C`
-    );
+    setLoaded(true);
+    setWeather({
+      name: response.data.name,
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+    });
   }
-  let apiKey = "eb9542c65e739e0fb25ade97c749e2aa";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
-  axios.get(url).then(displayForecast);
-  return (
-    <Vortex
-      visible={true}
-      height="100"
-      width="100"
-      ariaLabel="vortex-loading"
-      wrapperStyle={{}}
-      wrapperClass="vortex-wrapper"
-      colors={["black", "green", "green", "black", "green", "black"]}
-    />
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "eb9542c65e739e0fb25ade97c749e2aa";
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(url).then(displayForecast);
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  let form = (
+    <form className="App" onSubmit={handleSubmit}>
+      <input type="search" onChange={updateCity}></input>
+      <input type="submit"></input>
+    </form>
   );
+  if (loaded) {
+    return (
+      <div className="App">
+        {form}
+        <ul className="Description">
+          <li>{weather.name}</li>
+          <li>{weather.icon}</li>
+          <li>{Math.round(weather.temperature)}°C</li>
+          <li>{weather.description}</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>Wind: {weather.wind}km/h</li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
