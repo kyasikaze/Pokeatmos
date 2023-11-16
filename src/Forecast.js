9ef59b2e44760ef83eb00e0a38752e380e8a9e7c
kyasikaze <catarinapsantos98@gmail.com>
2023-11-16 11:33:10 +0100

    fixed function return

--- src/Forecast.js
+++ src/Forecast.js
@@ -24,6 +24,8 @@ export default function Forecast(props) {
                   <ForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
